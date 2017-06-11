fd62ae59d1a309201416c847185ead4eaedafdba
Matt Soucoup <msoucoup@codemilltech.com>
2017-06-11 12:51:23 -0500

    Added the mac app

---
+++ Forms4Mac.Mac/AppDelegate.cs
@@ -0,0 +1,44 @@
+﻿using AppKit;
+using Foundation;
+
+using Xamarin.Forms;
+using Forms4Mac;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Forms4Mac.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+
+        public AppDelegate()
+        {
+			var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+			var rect = new CoreGraphics.CGRect(200, 1000, 1024, 768);
+			_window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+			_window.Title = "The Garden Center";
+			_window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+        }
+
+        public override NSWindow MainWindow
+        {
+            get => _window;
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            // Insert code here to initialize your application
+            Forms.Init();
+            LoadApplication(new App());
+
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
